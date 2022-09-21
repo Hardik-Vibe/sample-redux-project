ebfd76639560a8da6e4db03537ff704505c91f01
Hardik Rajput <hardik.rajput14@yahoo.in>
2022-09-21 14:55:03 +0000

    connect API impl

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import Counter from "./feature/counter/counter";
+import CounterContainer from "./feature/counter/index";
+
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <Counter></Counter>
+      <CounterContainer></CounterContainer>
     </div>
   );
 }
